0b1d0c52e0e14b6c2611e8b3de6f061923f1688f
Sergey Komarov <anaxronik@gmail.com>
2020-06-02 00:34:47 +0500

    3 create article create page

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskDB.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text(300), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
@@ -13,13 +28,25 @@ def index_route():
 @app.route('/about')
 def about_route():
     print('=> New request on ABOUT route')
-
     return render_template('about.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user_route(name, id):
-    return 'User page: ' + str(name) + ' ' + str(id)
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article_route():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'ERROR'
+    else:
+        return render_template('create_article.html')
 
 
 if __name__ == "__main__":
