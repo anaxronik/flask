bf4ed1e97bffc70419e37ac4a6e11ea188bde3a7
Sergey Komarov <anaxronik@gmail.com>
2020-06-01 21:23:48 +0500

    2 add template html and css and bootstrap

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index_route():
-    return 'Hello from index'
+    print('=> New request on INDEX route')
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about_route():
-    return 'Hello from about'
+    print('=> New request on ABOUT route')
+
+    return render_template('about.html')
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user_route(name, id):
+    return 'User page: ' + str(name) + ' ' + str(id)
 
 
 if __name__ == "__main__":
